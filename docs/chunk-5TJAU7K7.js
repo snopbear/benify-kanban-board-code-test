0664c522597ef81ab3da1375df1a4e978145cccc
snopbear <snopbear@gmail>
2024-08-20 15:39:27 +0200

    initial-commit-responsive-based-publish

---
+++ docs/chunk-5TJAU7K7.js
@@ -0,0 +1,2 @@
+import{Ha as i,Ja as s,Q as o,ya as a,za as r}from"./chunk-UKG4J2TY.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-dashboard"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(a(0,"p"),i(1,` dashboard works!
+`),r())}});let t=e;return t})();export{m as DashboardComponent};
